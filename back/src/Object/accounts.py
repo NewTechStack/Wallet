8cc5de748a22b19ca1fe64461c7497d0049696b3
root <root@vps-a718f4ab.vps.ovh.net>
2022-01-22 11:38:53 +0000

    add

--- back/src/Object/accounts.py
+++ back/src/Object/accounts.py
@@ -1,4 +1,14 @@
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
+import json
+from hexbytes import HexBytes
+
+class HexJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, HexBytes):
+            return obj.hex()
+        return super().default(obj)
+
 try:
     from .rethink import get_conn, r
 except:
@@ -7,41 +17,54 @@ except:
 class Account:
     def __init__(self, usr_id = -1):
         self.w3 = Web3(Web3.HTTPProvider('https://rpc-mumbai.matic.today'))
+        self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         self.usr_id = str(usr_id)
         try:
-            self.red = get_conn().db("sell")
+            self.red = get_conn().db("wallet").table('accounts')
         except:
             self.red = None
 
     def status(self):
-        return [True, {'data': w3.eth.get_block('latest')}, None]
+        block = self.w3.eth.get_block('latest')
+        ret = json.dumps(dict(block), cls=HexJsonEncoder)
+        return [True, {'data': json.loads(ret)}, None]
 
     def create(self, name):
-        if usr_id == "-1":
+        if self.usr_id == "-1":
             return [False, "", 400]
+        self.w3.eth.account.enable_unaudited_hdwallet_features()
         acct, mnem = self.w3.eth.account.create_with_mnemonic()
         data = {
                 "usr_id": self.usr_id,
+                "name": name,
                 "address": str(acct.address),
                 "mnemonic": str(mnem)
         }
         res = dict(self.red.insert([data]).run())
-        id = res["generated_keys"][0] if id is None else id
+        id = res["generated_keys"][0]
         return [True, {"id": id, "address": str(acct.address), "mnemonic": str(mnem)} , None]
 
     def get_all(self):
         wallets = list(self.red.filter(
                 (r.row["usr_id"] ==  self.usr_id)
             ).run())
+        i = 0
+        while i < len(wallets):
+            del wallets[i]['mnemonic']
+            i += 1
         return [True, {'wallets': wallets}, None]
 
     def balance(self, account_addr):
-        account_addr = self.w3.toChecksumAddress(account_addr)
-        return [True, {'data': web3.eth.get_balance(str(addr))}, None]
+        account_addr = self.__address_from_str(account_addr)
+        if account_addr is False:
+            return [False, "Invalid wallet ID - user correspondance checksumAddress", 404]
+        return [True, {'data': self.web3.eth.get_balance(str(addr))}, None]
 
     def token_balance(self, account_addr, contract_addr):
         contract_addr = self.w3.toChecksumAddress(contract_addr)
-        account_addr = self.w3.toChecksumAddress(account_addr)
+        account_addr = self.__address_from_str(account_addr)
+        if account_addr is False:
+            return [False, "Invalid wallet ID - user correspondance checksumAddress", 404]
         minABI = [
           {
             "constant": True,
@@ -57,10 +80,29 @@ class Account:
             "outputs":[{"name":"","type":"uint8"}],
             "type":"function"
           }
-        ];
+        ]
         contract = self.w3.eth.contract(contract_addr, abi=minABI)
         balance = contract.functions.balanceOf(account_addr).call()
         return [True, {contract_addr: balance}, None]
 
     def __is_connected(self):
         return self.w3.isConnected()
+
+    def __address_from_id(self, wallet_id):
+        wallets = list(self.red.filter(
+                (r.row["usr_id"] == self.usr_id)
+                & (r.row["id"] == wallet_id)
+            ).run())
+        if len(wallet) != 1:
+            return False
+        return wallets[0]['address']
+
+    def __address_from_str(self, data):
+        wallet = self.__address_from_id(data)
+        if wallet is not False:
+            return wallet
+        try:
+            return self.w3.toChecksumAddress(data)
+        except:
+            pass
+        return False
