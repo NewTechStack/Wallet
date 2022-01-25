c42879db083a5140283d5850ab7b1b0e920fd72b
SCcagg5 <sccagg5@gmail.com>
2022-01-25 03:34:22 +0100

    update

--- back/src/Model/accounts.py
+++ back/src/Model/accounts.py
@@ -67,9 +67,7 @@ def contract_get_constructor(cn, nextc):
     return cn.call_next(nextc, err)
 
 def contract_exec_function(cn, nextc):
-    contract_type = cn.rt['contract']
-    contract_address = cn.rt[cn.rt['contract']] if cn.rt['contract'] in cn.rt else None
-    name = cn.rt[contract_address] if contract_address is not None else cn.rt[contract_type]
+    name = cn.rt[cn.rt[cn.rt['contract']]]
     err = check.contain(cn.pr, ["kwargs"])
     if not err[0]:
         return cn.toret.add_error(err[1], err[2])
