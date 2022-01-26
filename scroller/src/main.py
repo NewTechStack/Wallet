57c37e87ac8aad2331af984f06efe274e58d66a9
SCcagg5 <sccagg5@gmail.com>
2022-01-26 16:27:00 +0100

    adding

--- scroller/src/main.py
+++ scroller/src/main.py
@@ -63,19 +63,33 @@ class Scroller:
             if True:
                 self.meta = get_conn().db("wallet").table('transactions_meta')
                 self.transactions = get_conn().db("wallet").table('transactions')
+                self.accounts = get_conn().db("wallet").table('accounts')
+
                 break
                 print('waiting for DB')
+        address_list = [account['address'] for account in list(self.accounts.run())]
+        print(address_list)
         while True:
             for link in self.c:
                 latest = link[0].eth.get_block('latest')['number']
                 network = link[1]
-                lastchecked = list(self.meta.filter(r.row['network'] == network).run())
+                try:
+                    lastchecked = list(self.meta.filter(r.row['network'] == network).run())
+                except:
+                    continue
                 if len(lastchecked) == 0:
                     res = dict(self.meta.insert([{'network': network, 'lastchecked': latest}]).run())
                 else:
+
                     lastchecked = lastchecked[0]['lastchecked']
                     print(f"{network}: from {lastchecked} to {latest}")
                     while lastchecked < latest:
+                        block = self.link.eth.get_block(actual, full_transactions=True)
+                        for transaction in block['transactions']:
+                            recei = transaction['to']
+                            expe = transaction['from']
+                            if recei in address_list or expe in address_list:
+                                print('found', transaction)
                         lastchecked += 1
                         self.meta.filter(r.row['network'] == link[1]).update({'lastchecked': latest}).run()
             time.sleep(30)
