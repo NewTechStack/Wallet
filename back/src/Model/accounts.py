7103e0efef89f973c583f0dac2af8708fafcc9bd
SCcagg5 <sccagg5@gmail.com>
2022-01-26 11:47:22 +0100

    adding

--- back/src/Model/accounts.py
+++ back/src/Model/accounts.py
@@ -76,5 +76,5 @@ def contract_exec_function(cn, nextc):
 
 def contract(cn, nextc):
     id = cn.get.get('id')
-    err = Contract().get_contract(id)
+    err = Contract('').get_contract(id)
     return cn.call_next(nextc, err)
