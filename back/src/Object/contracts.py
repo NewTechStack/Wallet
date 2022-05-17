6f77ed5cd49bcde7035bcbcd9a585514d9a089a7
SCcagg5 <sccagg5@gmail.com>
2022-05-17 15:58:47 +0200

    fix additionnal gas

--- back/src/Object/contracts.py
+++ back/src/Object/contracts.py
@@ -1,6 +1,6 @@
 from web3 import Web3, exceptions, middleware
 from web3.middleware import geth_poa_middleware
-from web3.gas_strategies.time_based import construct_time_based_gas_price_strategy
+from web3.gas_strategies.time_based import fast_gas_price_strategy
 from hexbytes import HexBytes
 import requests
 import os
@@ -63,7 +63,9 @@ class W3:
             return [False, "invalid connection argument", 400]
         provider = self.networks[self.network_type][self.network]['rpc']
         self.link = Web3(Web3.HTTPProvider(provider))
+        self.link.eth.setGasPriceStrategy(fast_gas_price_strategy)
         self.link.middleware_onion.inject(geth_poa_middleware, layer=0)
+        self.link.middleware_stack.add(latest_block_based_cache_middleware)
         self.link.eth.account.enable_unaudited_hdwallet_features()
         self.unit = 'ETH' if self.network_type == 'ether' else 'MATIC' if self.network_type == 'polygon' else ''
         return [True, f"Connected to {provider}", None]
@@ -79,6 +81,7 @@ class W3:
         if gas_cost is None:
             return [False, "Invalid logic", 400]
         build = None
+
         gas_price = self.link.eth.generate_gas_price() * mult_gas
         for _ in range(10):
             try:
